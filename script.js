c1459c1da0f1e54bc6a987f1caac01212ef75eb4
Elgun Mehdi <elgun88@gmail.com>
2023-12-11 19:06:52 -0600

    Add regexp for pwd & email & pwd confirmation logic

--- script.js
+++ script.js
@@ -3,6 +3,19 @@
 const form = document.querySelector('.sign-up-form');
 const inputLabels = document.querySelectorAll('.input-label');
 const inputs = document.querySelectorAll('.input');
+const password = document.querySelector('#password');
+const passwordConfirm = document.querySelector('#password-confirm');
+
+const confirmPassword = function (pwd, confirmPwd) {
+  confirmPwd.classList.remove('isNotValid');
+  confirmPwd.classList.remove('isValid');
+
+  if (!pwd.validity.valid) return;
+
+  confirmPwd.classList.add(
+    `${pwd.value === confirmPwd.value ? 'isValid' : 'isNotValid'}`
+  );
+};
 
 inputs.forEach(input => {
   input.setAttribute('value', input.value);
@@ -15,13 +28,16 @@ form.addEventListener('keyup', function (e) {
   target.attributes.value.value = target.value;
 });
 
-document.addEventListener('focusin', function (e) {
+form.addEventListener('focusin', function (e) {
   const target = e.target;
   if (!target.classList.contains('input')) return;
 
   inputLabels.forEach(label => {
     if (target.id === label.attributes.for.value) label.classList.add('active');
   });
+
+  // target.classList.remove('isNotValid');
+  // target.classList.remove('isValid');
 });
 
 form.addEventListener('focusout', function (e) {
@@ -37,4 +53,10 @@ form.addEventListener('focusout', function (e) {
       });
     }
   });
+
+  // CONFIRM PASSWORD WHEN FOCUS OUT
+  if (target.type !== 'password') return;
+  confirmPassword(password, passwordConfirm);
 });
+
+// ON SUBMIT BUTTON - CONFIRM PASSWORD SHOULD HAVE ISVALID CLASS!!!
